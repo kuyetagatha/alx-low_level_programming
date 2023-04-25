2ba844fb974ed249b9f24f29664d213d6730a8a8
kuyetagatha <paulagatha45@gmail.com>
2023-04-25 06:02:38 -0700

    _putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,8 +4,11 @@
  * main - prints _putchar and a new line
  * Return: Always 0
  */
+
 int main(void)
+
 {
+
 	_putchar(',');
 	_putchar('p');
 	_putchar('u');
@@ -15,6 +18,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
