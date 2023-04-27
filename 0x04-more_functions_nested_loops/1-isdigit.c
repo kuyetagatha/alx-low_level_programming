40137c83d6866854a74de867d580b323ee7366d1
kuyetagatha <paulagatha45@gmail.com>
2023-04-27 13:48:07 -0700

    c-more functions, more nested loops

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,18 +1,17 @@
-#include "main.h
+#include "main.h"
 
 /**
- * _isdigit - Check if a character is a digit
- * @x: The number to be checked
- * Return: 1 for a checker that will be a digit or 0 for any else
+ * _isdigit - Check if numbers are 0 -9
+ * @x: char to be checked
+ *
+ * Return: 0 or 1
  */
 
-int _isdigit(int x)
+int _isdigit(int c)
 
 {
-
-	if (x >= 48 && x <= 57)
-	{
-	return (1);
-	}
-	return (0);
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
 }
--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,16 +1,12 @@
 #include "main.h"
-
 /**
- * mul - Multiplies two integers
- * @x: The first number to multiply
- * @y: The second number to multiply with first number
- * Return: The result of multiplication between x paron and y paron
+ * mul - Multiplies 2 integers
+ * @x! The first number to multiply
+ * @y! The second integers
+ * Return: addition of the multiplication
  */
 
-int mul(int x, int y)
-
+int mul(int a, int b)
 {
-
-	return (x * y);
-
+	return (a * b);
 }
