8242dc027d5033c97d8abc92edcf48631a6b8a5d
kuyetagatha <paulagatha45@gmail.com>
2023-04-27 15:54:52 -0700

    ./9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
- * main - prints the numbers from 1 to 100, followed by a new line
- * but for multiples of three prints fizz instead of the number
- * and for the multiples of five prints buzz
- * Return: Always 0 (Success)
- */
+* main - prints the numbers from 1 to 100, followed by a new line
+* but for multiples of three prints fizz instead of the number
+* and for the multiples of five prints buzz
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	int i;
@@ -15,19 +15,19 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("Fizz");
+			printf(" Fizz");
 		} else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf("Buzz");
+			printf(" Buzz");
 		} else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz");
+			printf(" FizzBuzz");
 		} else if (i == 1)
 		{
 			printf("%d", i);
 		} else
 		{
-			printf("%d", i);
+			printf(" %d", i);
 		}
 
 	}
--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * _putchar.c - writes the character c to stdout
- * @c The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned and error is set appropriately.
- */
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned and emmo is set appropriately.
+*/
 int _putchar(char c)
 {
 	return(write (1, &c, 1));
