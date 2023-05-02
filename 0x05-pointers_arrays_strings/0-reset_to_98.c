2ac1ccf796b09c78ddac54d8d4f59e97da7a8237
kuyetagatha <paulagatha45@gmail.com>
2023-05-02 09:38:45 -0700

    ./2-strlen

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,12 @@
 
 /**
  * reset_to_98 - updates the value of the variable the pointer points to 98
- * @n: Pointer to the variable to update
- * Return: n
+ * @n: input Pointer to the variable to update
+ * main: This function takes a pointer to an integer variable and updates
+ * the value of the variable to 98.
+ * Return: void
  * /
 void reset_to_98(int *n)
 {
-	*n = 98;
+*n = 98;
 }
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,14 +4,17 @@
  * Swap_int - Swaps the values of two integers.
  * @a: The first integer to be swapped.
  * @b: The second integer to be swapped.
- *
- * Return: n
+ * main: This function swaps the values of two integers
+ * Return: void
  */
 
+int main(void);
 void swap_int(int *a, int *b)
 /* the function that swaps the values of two integers. */
 {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+int m;
+
+m = *a;
+*a = *b;
+*b = m;
 }
