5e34f137ef4b3f6fff4664e4e9cb244c1383bce5
kuyetagatha <paulagatha45@gmail.com>
2023-05-10 09:26:18 -0700

    0x08-recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,39 +1,35 @@
 #include "main.h"
+#include <stdio.h>
 
-int actual_sqrt_recursion(int n, int i);
+int _sqrt(int n, int i);
+
 /**
- * _sqrt_recursion - return the natural square root of a numbeer
- * @n: number to calculate the square root of 
+ * _sqrt_recursion - Returns the natural square root of a numbeer
+ * @n: number to calculate the natural square root
  *
- * Return: the resulting square root
+ * Return: the natural square root
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-	return (actual_sqrt_recursion(n, 0));
+	return (_sqrt(n, 1));
 }
 
 /**
- * actual_sqrt_recursion - recurses to find the natural
- * square root of a number
+ * _sqrt - calculates natural square root
  * @n: number to calculate the square root of
- * @i: iterator
+ * @i: iterate number
  *
- * Return: the resulting square root
+ * Return: the natural square root
  */
-int actual_sqrt_recursion(int n, int i)
+int _sqrt(int n, int i)
 {
-	if (1 * i > n)
+	int sqrt = i * i;
+
+	if (sqrt > n)
 		return (-1);
-	if (1 * i == n)
+
+	if (sqrt == n)
 		return (1);
-	return (actual_sqrt_recursion(n, i +1));
-}
-{
-	if (1 * i > n)
-		return (-1);
-	if (1 * i == n)
-		return (1);
-	return (actual_sqrt_recursion(n, i + 1));
+
+	return (_sqrt(n, i + 1));
 }
