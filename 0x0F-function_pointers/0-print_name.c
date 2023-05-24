1f9fdf50ec814a6676432ce59e783710ccf099f6
kuyetagatha <paulagatha45@gmail.com>
2023-05-24 08:36:36 -0700

    corrected commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,5 @@
 #include "function_pointers.h"
 #include <stdio.h>
-
 /**
  * print_name - print name using pointer to function
  * @name: string to add
@@ -11,6 +10,5 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
-
 	f(name);
 }
