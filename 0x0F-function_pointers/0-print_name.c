399664f160941d2b896da116a75ae617b8bd66c1
kuyetagatha <paulagatha45@gmail.com>
2023-05-24 10:16:59 -0700

    function_pointers.h is the header file

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,14 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
- * print_name - print name using pointer to function
- * @name: string to add
+ * print_name - prints a name
+ * @name: The name to prints
  * @f: pointer to function
+ *
  * Return: 0
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-	f(name);
+if (name != NULL && f != NULL)
+f(name);
 }
