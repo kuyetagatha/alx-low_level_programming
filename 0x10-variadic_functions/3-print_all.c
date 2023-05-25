48130d63d08c5d2595eef4ee183c3ab1f05ba7cf
kuyetagatha <paulagatha45@gmail.com>
2023-05-25 14:38:38 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,20 +7,20 @@
 */
 void print_all(const char * const format, ...)
 {
-int a = 0;
+int i = 0;
 char *str, *sep = "";
 va_list list;
 va_start(list, format);
 if (format)
 {
-while (format[a])
+while (format[i])
 {
-switch (format[a])
+switch (format[i])
 {
 case 'c':
 printf("%s%c", sep, va_arg(list, int));
 break;
-case 'a':
+case 'i':
 printf("%s%d", sep, va_arg(list, int));
 break;
 case 'f':
@@ -33,11 +33,11 @@ str = "(nil)";
 printf("%s%s", sep, str);
 break;
 default:
-a++;
+i++;
 continue;
 }
 sep = ", ";
-a++;
+i++;
 }
 }
 printf("\n");
