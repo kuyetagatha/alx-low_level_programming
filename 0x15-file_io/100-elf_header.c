58557883e15d0542dff122afd14e8231af0f3af2
Paul Agatha <paulagatha45@gmail.com>
2023-07-13 06:36:35 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -254,8 +254,8 @@ void close_elf(int elf)
 	if (close(elf) == -1)
 	{
 		dprintf(STDERR_FILENO,
-				"Error: Can't close fd %d\n", elf);
-		exit(98);
+			"Error: Can't close fd %d\n", elf);
+	exit(98);
 	}
 }
 
@@ -272,7 +272,6 @@ void close_elf(int elf)
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-
 	Elf64_Ehdr *header;
 	int o, r;
 
